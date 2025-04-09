9774258a8258db218f5b441793f123704f0df8a5
Ivan Lebedev <iamil50113@gmail.com>
2025-04-10 01:50:36 +0300

    update

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
     // Config options...
     // Server-side render by default, to enable SPA mode set this to `false`
     ssr: true,
-    prerender: ["", "/about", "/cases", "/contacts", "/contactus", "/licenses", "/reviews"],
+    prerender: ["", "/about", "/cases", "/contacts", "/contactus", "/licenses", "/reviews", "/services"],
 } satisfies Config;
